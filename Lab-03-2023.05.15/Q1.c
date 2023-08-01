7ea183e079d9398616c752bdf88234b7dd45cfef
sahan8 <113032980+sahan8@users.noreply.github.com>
2023-08-01 11:32:00 +0530

    Delete Lab-03-2023.05.15 directory

--- Lab-03-2023.05.15/Q1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int age;
-    printf("HI, HOW OLD ARE YOU?\n");
-    scanf("%d", &age);
-    printf("\n\n WELCOME %d\nLET'S BE FRIENDS!\n", age);
-    return 0;
-}
